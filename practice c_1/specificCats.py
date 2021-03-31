f47abf76defd80633cdf2061f6c8f95a95137b14
ulvim <umikayilov@gmail.com>
2021-04-01 00:18:14 +0400

    Added file for c_1.8.1 task

---
+++ practice c_1/specificCats.py
@@ -0,0 +1,14 @@
+from cat import Cat
+
+cat_1 = Cat("Baron","male", 2)
+
+print("cat_1 name ", cat_1.get_name())
+print("cat_1 sex ", cat_1.get_sex())
+print("cat_1 age", cat_1.get_age())
+
+
+cat_2 = Cat("Sam", "male", 2)
+
+print("cat_2 name ", cat_2.get_name())
+print("cat_2 sex ", cat_2.get_sex())
+print("cat_2 age", cat_2.get_age())
